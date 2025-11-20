76b1804948e615db3964b8b08ae854514b13b860
Soham0798 <sohambz.patwardhan@gmail.com>
2025-11-20 10:25:22 +0530

    Update page.tsx

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -115,7 +115,7 @@ export default function TicketsPage() {
     <>
       <NavBar />
       <main className=" bg-gradient-to-br from-gray-50 to-blue-50 py-10 px-4">
-        <div className="max-w-6xl mx-auto">
+        <div className="max-w-md p-4 w-full mx-auto">
           <div className="text-center mb-10">
             <h1 className="text-4xl font-bold text-gray-800 mb-3">My Tickets</h1>
             <p className="text-gray-600">Manage and view your bus tickets</p>
@@ -205,6 +205,7 @@ export default function TicketsPage() {
     </>
   );
 }
+
 
 
 
