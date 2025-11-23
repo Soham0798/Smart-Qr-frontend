0807a6ad1c5aa144ccaa54a5129fb6252fc2f893
Soham0798 <sohambz.patwardhan@gmail.com>
2025-11-23 14:28:37 +0530

    Update page.tsx

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -9,7 +9,7 @@ export default function ProfilePage() {
   useEffect(() => {
     async function fetchUser() {
       try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/me`, {
+        const res = await fetch(`https://smart-qr-backend-production.up.railway.app/users/me`, {
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${localStorage.getItem("token")}`,
@@ -92,3 +92,4 @@ export default function ProfilePage() {
     </>
   );
 }
+
